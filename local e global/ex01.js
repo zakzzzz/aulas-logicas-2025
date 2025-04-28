4ec597331b7c7ce5803cf98e304dc336782982f7
Isac <isac.f.ferreira@aluno.senai.br>
2025-04-28 10:49:48 -0300

    ex10

---
+++ local e global/ex01.js
@@ -0,0 +1,10 @@
+let contador0 = 0
+function contador(){
+    contador0++
+    return contador0
+}
+console.log(contador())
+console.log(contador())
+console.log(contador())
+console.log(contador())
+console.log(contador())
